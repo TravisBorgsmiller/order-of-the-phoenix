dd2ca1365a8f8ae90fc70d7c3a56b29c05671bca
Travis Borgsmiller <travisborgsmiller@gmail.com>
2020-05-19 12:14:07 -0500

    add service function to make member objects

--- app/services/hp_service.rb
+++ app/services/hp_service.rb
@@ -1,24 +1,39 @@
 class HpService
 
 
-  def get_members
-    params = { house: 'gryffindor', orderOfThePhoenix: true }
+  # def get_members(house)
+  #   params = { house: house, orderOfThePhoenix: true }
+  #
+  #   get_json('/characters', params)
+  # end
 
-    get_json('/characters', params)
-  end
+  # private
 
-  private
+  # def get_json(url, params)
+  #   response = conn.get(url, params)
+  #   JSON.parse(response.body, symbolize_names: true)
+  # end
 
-  def get_json(url, params)
-    response = conn.get(url, params)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def conn
-    Faraday.new(url: 'https://www.potterapi.com/v1') do |f|
-      f.adapter Faraday.default_adapter
-      f.params[:key] = ENV['HP_API_KEY']
+  def get_object(house)
+    response = get_members(house)
+    members = []
+    response.each do |member|
+      if member[:patronus]
+        members << Member.new(member[:name], member[:role], member[:house], member[:patronus])
+      else
+        members << Member.new(member[:name], member[:role], member[:house])
+      end
     end
+    members
+  end
+
+  def get_members(house)
+    response = Faraday.get('https://www.potterapi.com/v1/characters') do |f|
+      f.params[:key] = ENV['HP_API_KEY']
+      f.params[:house] = house
+      f.params[:orderOfThePhoenix] = true
+    end
+    JSON.parse(response.body, symbolize_names: true)
   end
 
 end
